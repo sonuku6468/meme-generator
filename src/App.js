283d2bba18f20dda11e5751e54d084c0d69fd41c
sonuku6468 <108977415+sonuku6468@users.noreply.github.com>
2022-12-02 14:54:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Meme from './components/Meme';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+        <Meme/>
+      
+    </div>
+  );
+}
+
+export default App;
